dc59ec84cd5ce7b741a0018e5b9db4ce97bc84e1
paidejav000-web <paidejav000@graftonschools.org>
2026-04-10 14:24:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 function openServer(type) {
-  if (type === "war") {
-    alert("War System: Declare wars, manage alliances, track battles.");
-  }
+  window.location.href = type + ".html";
+}
 
-  if (type === "lifesteal") {
-    alert("LifeSteal: Players steal hearts when killing others.");
-  }
-
-  if (type === "anarchy") {
-    alert("Anarchy: No rules. Full chaos enabled.");
-  }
-
-  if (type === "peaceful") {
-    alert("Peaceful: No PvP. Build freely.");
-  }
-
-  if (type === "template") {
-    alert("Template: Create your own server mode.");
-  }
+function goBack() {
+  window.location.href = "index.html";
 }
